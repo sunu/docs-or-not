734d98c908e57ab586354a41a663d88e461c8444
Tarashish Mishra <sunu0000@gmail.com>
2019-10-28 21:10:23 +0530

    add option to filter inplace ie delete non-doc images from source

--- Makefile
+++ Makefile
@@ -2,4 +2,7 @@ build:
 	docker build -t docs-or-not .
 
 filter:
-	docker run -v ${src}:/src-dir -v ${dest}:/dest-dir docs-or-not /src-dir /dest-dir
+	docker run -v ${src}:/src-dir -v ${dest}:/dest-dir docs-or-not filter /src-dir /dest-dir
+
+filter_inplace:
+	docker run -v ${src}:/src-dir -v ${dest}:/dest-dir docs-or-not filter_inplace /src-dir /dest-dir
--- cli.py
+++ cli.py
@@ -5,6 +5,7 @@ from io import BytesIO
 from shutil import copyfile
 import sys
 import warnings
+import collections
 
 from fastai.vision import load_learner, open_image
 
@@ -15,17 +16,27 @@ DOC_THRESHOLD = 0.5
 warnings.filterwarnings("ignore", category=UserWarning, module="torch.nn.functional")  # noqa
 
 
-def check_if_doc(path):
+Result = collections.namedtuple("Result", "is_img, is_doc_img")
+
+
+def check_if_doc_img(path):
     with open(path, 'rb') as fp:
-        img = BytesIO(fp.read())
-        try:
-            img = open_image(img)
-        except OSError:
-            return False
+        img = check_if_img(fp)
+        if img is False:
+            return Result(is_img=False, is_doc_img=False)
         _, _, losses = learner.predict(img)
         predictions = dict(zip(learner.data.classes, map(float, losses)))
         if predictions[DOC_LABEL] > DOC_THRESHOLD:
-            return True
+            return Result(is_img=True, is_doc_img=True)
+        return Result(is_img=True, is_doc_img=False)
+
+
+def check_if_img(fp):
+    img = BytesIO(fp.read())
+    try:
+        img = open_image(img)
+        return img
+    except OSError:
         return False
 
 
@@ -34,7 +45,8 @@ def filter_dir(path, outdir, root_src):
     for item in os.listdir(root):
         path = root.joinpath(item)
         if os.path.isfile(path):
-            if check_if_doc(path):
+            res = check_if_doc_img(path)
+            if res.is_doc_img:
                 rel_path = path.relative_to(root_src)
                 dest = pathlib.Path(outdir).joinpath(rel_path)
                 st = os.stat(path)
@@ -49,12 +61,31 @@ def filter_dir(path, outdir, root_src):
             filter_dir(root.joinpath(item), outdir, root_src)
 
 
+def filter_inplace(path, root_src):
+    root = pathlib.Path(path)
+    for item in os.listdir(root):
+        path = root.joinpath(item)
+        if os.path.isfile(path):
+            res = check_if_doc_img(path)
+            if res.is_img and not res.is_doc_img:
+                os.remove(path)
+                rel_path = path.relative_to(root_src)
+                print(f'Deleted $SRC/{rel_path}')
+        if os.path.isdir(path):
+            filter_inplace(root.joinpath(item), root_src)
+
+
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Need 2 arguments: the source directory and the destination")
+    if len(sys.argv) != 4:
+        print("Need 3 arguments: the command (filter or filter_inplace), the source directory and the destination")  # noqa
         sys.exit(0)
-    source = pathlib.Path(sys.argv[1])
+    source = pathlib.Path(sys.argv[2])
     assert source.exists()
-    dest = pathlib.Path(sys.argv[2])
+    dest = pathlib.Path(sys.argv[3])
     assert dest.exists()
-    filter_dir(source, dest, source)
+    command = sys.argv[1]
+    assert command in ('filter', 'filter_inplace')
+    if command == 'filter':
+        filter_dir(source, dest, source)
+    elif command == 'filter_inplace':
+        filter_inplace(source, source)
